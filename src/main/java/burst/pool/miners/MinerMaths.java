94f6ce78bc8d09dd2d77b8b4ce52efc5c4f6be87
BalazsDev <nabali87@gmail.com>
2021-04-30 08:19:16 +0200

    Update version to 1.6.3

--- src/main/java/burst/pool/miners/MinerMaths.java
+++ src/main/java/burst/pool/miners/MinerMaths.java
@@ -8,7 +8,7 @@ import java.math.BigInteger;
 public class MinerMaths {
     private final Logger logger = LoggerFactory.getLogger(MinerMaths.class);
 
-    private static final long GenesisBaseTarget = 18325193796L;
+    public static final long GENESIS_BASE_TARGET = 18325193796L;
     private final double[] alphas;
 
     public MinerMaths(int nAvg, int nMin) {
@@ -23,10 +23,10 @@ public class MinerMaths {
         }
         alphas[nAvg-1] = 1d;
     }
-    
+
     /**
      * Tries to estimate the total capacity of the account, given a hitSum and number of confirmations
-     *  
+     *
      * @param nConf
      * @param hitSum
      * @return the estimated total capacity
@@ -38,12 +38,12 @@ public class MinerMaths {
     public double estimatedEffectivePlotSize(int originalNConf, int nConf, BigInteger hitSum) {
         return estimatePlotSize(alpha(originalNConf), nConf, hitSum);
     }
-    
+
     private double estimatePlotSize(double alphaValue, int nConf, BigInteger hitSum) {
         if (hitSum.compareTo(BigInteger.ZERO) == 0) {
             throw new ArithmeticException();
         }
-        double plotSize =  alphaValue * 240d * (((double)nConf)-1d) * (double)GenesisBaseTarget / hitSum.doubleValue();
+        double plotSize =  alphaValue * 240d * (((double)nConf)-1d) * (double)GENESIS_BASE_TARGET / hitSum.doubleValue();
         if (Double.isInfinite(plotSize) || Double.isNaN(plotSize)) {
             logger.debug("Calculated impossible plot size. alpha: " + alphaValue + ", nConf: " + nConf + ", hitSum: " + hitSum);
             throw new ArithmeticException();
